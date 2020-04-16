66ad5a72442d049bdd2bbcc132d43764df845f79
TrillGates <wx_f93f2baa60e84b219009ff090d32fbf0@git.code.tencent.com>
2020-04-16 23:30:54 +0800

    adjust discovery page style.

--- pages/discovery/index.vue
+++ pages/discovery/index.vue
@@ -19,22 +19,24 @@
           </div>
           <div class="float-left item-right-info">
             <div class="item-title">
-              <span v-text="item.title">
+              <a :href="item.coupon_click_url" target="_blank">
+                 <span v-text="item.title">
 
               </span>
+              </a>
             </div>
             <div class="prise-info">
-              <span v-text="'原价:'+item.zk_final_price"></span>
-              <span>券后价:</span>
-              <span v-text="item.zk_final_price-item.coupon_amount"></span>
+              <span class="original-prise" v-text="'原价:'+item.zk_final_price"></span>
+              <span class="off-prise">券后价: <span v-text="to2Bit(item.zk_final_price-item.coupon_amount)"></span></span>
+
             </div>
             <div class="sell-info">
               <span v-text="item.volume"></span>
               <span>·</span>
-              <span>已购买</span>
+              <span>人已购买</span>
             </div>
           </div>
-          <el-button type="danger" size="small">领券购买</el-button>
+          <a target="_blank" :href="item.coupon_click_url" class="discovery-buy-btn" type="danger" size="small">领券购买</a>
         </div>
       </div>
       <div class="discovery-right-part float-left">
@@ -47,6 +49,11 @@
   import api from '../../utils/api';
 
   export default {
+    methods: {
+      to2Bit(num) {
+        return num.toFixed(2);
+      }
+    },
     async asyncData() {
       let categoriesResult = await api.getCategories();
       console.log(categoriesResult);
@@ -70,6 +77,33 @@
 </script>
 <style>
 
+
+  .original-prise {
+    color: #999999;
+    text-decoration: line-through;
+  }
+
+  .off-prise {
+    color: orangered;
+  }
+
+  .discovery-buy-btn:hover {
+    background-color: orangered;
+    border-color: orangered;
+  }
+
+  .discovery-buy-btn {
+    text-decoration: none;
+    padding: 8px 20px;
+    border-radius: 5px;
+    position: absolute;
+    bottom: 20px;
+    background-color: #F56C6C;
+    border-color: #F56C6C;
+    color: white;
+    right: 20px;
+  }
+
   .discovery-category-active {
     color: #fff;
     background: #ff4500;
@@ -79,6 +113,20 @@
     margin-top: 20px;
   }
 
+  .sell-info {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    color: #47494e;
+    font-size: 16px;
+  }
+
+
+  .prise-info {
+    font-size: 16px;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    font-weight: 600;
+  }
 
   .discovery-left-part li:hover {
     background: #ff4500;
@@ -105,11 +153,39 @@
     box-shadow: 0px 5px 10px #d4d4d4;
   }
 
+
+  .item-left-cover img {
+    border-radius: 5px;
+  }
+
+  .item-title a {
+    font-size: 22px;
+    color: #47494e;
+    text-decoration: none;
+  }
+
+  .item-title {
+    cursor: pointer;
+    max-width: 500px;
+    font-weight: 600;
+  }
+
+  .item-right-info {
+    margin-left: 10px;
+  }
+
+  .discovery-content-item {
+    margin-bottom: 10px;
+    position: relative;
+    padding: 10px;
+    background: #fff;
+    box-shadow: 0px 5px 10px #d4d4d4;
+  }
+
   .discovery-center-part {
     width: 710px;
     margin-left: 10px;
     margin-right: 10px;
-    background: antiquewhite;
   }
 
   .discovery-right-part {
