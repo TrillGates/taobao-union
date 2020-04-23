08c6f8c40573d5d81efeda2980ccc2b3c317829c
TrillGates <wx_f93f2baa60e84b219009ff090d32fbf0@git.code.tencent.com>
2020-04-23 13:43:41 +0800

    to ticket page.

--- pages/onSell/index.vue
+++ pages/onSell/index.vue
@@ -2,31 +2,33 @@
   <div class="center-box">
     <div class="onSell-content-box clear-fix">
       <div class="onSell-content-item float-left" v-for="(item,index) in contentList" :key="index">
-        <div class="onSell-item-cover">
-          <el-image
-            style="width: 263px; height: 263px"
-            :src="item.pict_url+'_270x270xzq90.jpg_.webp'"
-            fit="cover"></el-image>
-          <div class="onSell-off-prise">
-            省{{item.coupon_amount}}元
+        <a :href="item.coupon_click_url" target="_blank">
+          <div class="onSell-item-cover">
+            <el-image
+              style="width: 263px; height: 263px"
+              :src="item.pict_url+'_270x270xzq90.jpg_.webp'"
+              fit="cover"></el-image>
+            <div class="onSell-off-prise">
+              省{{item.coupon_amount}}元
+            </div>
           </div>
-        </div>
-        <div class="onSell-item-prise">
-          <span class="onSell-item-final-prise" v-text="'￥'+item.zk_final_price"></span>
-          <span>
+          <div class="onSell-item-prise">
+            <span class="onSell-item-final-prise" v-text="'￥'+item.zk_final_price"></span>
+            <span>
             券后价:
             <span v-text="to2Bit(item.zk_final_price-item.coupon_amount)"></span>
           </span>
-        </div>
-        <div class="onSell-item-volume">
-          <span v-text="item.volume"></span>
-          ·人已购买
-        </div>
-        <div class="onSell-item-title">
-          <p v-text="item.title">
+          </div>
+          <div class="onSell-item-volume">
+            <span v-text="item.volume"></span>
+            ·人已购买
+          </div>
+          <div class="onSell-item-title">
+            <p v-text="item.title">
 
-          </p>
-        </div>
+            </p>
+          </div>
+        </a>
       </div>
     </div>
   </div>
@@ -55,6 +57,7 @@
     }
   }
 </script>
+
 <style>
 
   .onSell-item-title {
@@ -85,16 +88,21 @@
   }
 
   .onSell-content-box {
+    margin-bottom: 20px;
     margin-top: 20px;
   }
 
   .onSell-content-item:hover {
     border: #c9302c solid 1px;
+  }
 
+  .onSell-content-item > a {
+    text-decoration: none;
   }
 
   .onSell-content-item {
     width: 275px;
+    cursor: pointer;
     margin: 5px;
     padding: 5px;
     border: #dfdfdf 1px solid;
