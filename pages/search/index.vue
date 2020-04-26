35f81f935a936f12d2433d76e9e20585a905e387
TrillGates <wx_f93f2baa60e84b219009ff090d32fbf0@git.code.tencent.com>
2020-04-26 22:22:02 +0800

    adjust search result style.

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -3,20 +3,24 @@
     <div class="search-content-box clear-fix">
       <div class="search-content-item float-left"
            v-for="(item,index) in searchResult" :key="index">
-        <div class="search-content-cover">
-          <el-image :src="item.pict_url"></el-image>
-        </div>
-        <div class="search-content-prise-info">
-          <span v-text="'￥'+item.zk_final_price"></span>
-          <span>券后价: <span v-text="to2Bit(item.zk_final_price-item.coupon_amount)"></span>
+        <a :href="item.coupon_share_url===null?item.url:item.coupon_share_url" target="_blank">
+          <div class="search-content-cover">
+            <el-image :src="item.pict_url"></el-image>
+            <span class="off-prise-tips" v-if="item.coupon_amount!==null" v-text="'省'+item.coupon_amount+'元'"></span>
+          </div>
+          <div class="search-content-prise-info">
+            <span class="original-prise" v-text="'￥'+item.zk_final_price"></span>
+            <span>券后价: <span v-text="to2Bit(item.zk_final_price-item.coupon_amount)"></span>
           </span>
-        </div>
-        <div class="search-content-volume">
-          <span v-text="item.volume+'·已购买'"></span>
-        </div>
-        <div class="search-content-title">
-          <p v-text="item.title"></p>
-        </div>
+          </div>
+          <div class="search-content-volume">
+            <span v-text="item.volume+'·已购买'"></span>
+          </div>
+          <div class="search-content-title">
+            <p v-text="item.title"></p>
+          </div>
+        </a>
+
       </div>
     </div>
   </div>
@@ -61,8 +65,65 @@
 </script>
 <style>
 
+  .search-content-cover {
+    position: relative;
+  }
+
+  .off-prise-tips {
+    position: absolute;
+    background: #c9302c;
+    color: #fff;
+    padding: 5px 18px;
+    top: 0;
+    right: 0;
+  }
+
+  .search-content-title {
+    font-size: 16px;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    overflow: hidden;
+    color: #000;
+  }
+
+  .search-content-volume {
+    margin-bottom: 5px;
+    color: #7f828b;
+    font-size: 16px;
+  }
+
+  .search-content-prise-info .original-prise {
+    text-decoration: line-through;
+  }
+
+  .search-content-prise-info {
+    color: orangered;
+    font-size: 18px;
+    font-weight: 600;
+    margin-top: 5px;
+    margin-bottom: 5px;
+  }
+
+  .search-content-box {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  .search-content-item a {
+    text-decoration: none;
+  }
+
+  .search-content-item:hover {
+    border: #c63632 1px solid;
+  }
+
   .search-content-item {
+    cursor: pointer;
     width: 275px;
+    margin: 5px;
+    padding: 5px;
+    border: #dfdfdf 1px solid;
   }
 
   .search-content-cover .el-image {
